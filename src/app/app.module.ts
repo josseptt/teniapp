d60235a666e7df99ac2327c0132c470f046302a3
Jose Delgado <josseptt@gmail.com>
2018-06-24 11:09:35 -0600

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialDashboardComponent } from './material-dashboard/material-dashboard.component';
+import { MatGridListModule, MatCardModule, MatMenuModule, MatIconModule, MatButtonModule } from '@angular/material';
+import {RouterModule, Routes} from '@angular/router';
+
+
+const routes: Routes = [
+  { path: 'dashboard', loadChildren: './teniapp/teniapp.module#TeniappModule'},
+  { path: '**', redirectTo: 'dashboard'}
+];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MaterialDashboardComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatGridListModule,
+    MatCardModule,
+    MatMenuModule,
+    MatIconModule,
+    MatButtonModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
